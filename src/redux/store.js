2a466a62edc5de4e96fb6c403b871c18a96f9adb
Ritesh Kumar <ritesh8002830249@gmail.com>
2026-02-02 01:02:33 +0530

    code added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "./features/counterSlice";
+import counterSlicereducer from "./features/counterSlice";
 
 export  const store=configureStore({
     reducer:{
+        counter:counterSlicereducer
 
     }
 })
